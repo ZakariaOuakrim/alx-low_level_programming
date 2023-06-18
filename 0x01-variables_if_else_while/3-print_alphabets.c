e6bb4f667ae708fc36072fe9c1051e7d594c2dc6
ZakariaOuakrim <zakarialukyguy@gmail.com>
2023-06-18 14:25:34 +0000

    qjbsdkqbsd

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,19 +7,13 @@
  */
 int main(void)
 {
-	char t1[26] = "abcdefghijklmnopqrstuvwxyz";
-
-	char t2[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char t1[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 52; i++)
 	{
-		putchar(t1[26]);
-	}
-	for (i = 0; i < 26; i++)
-	{
-		putchar(t2[26]);
+		putchar(t1[i]);
 	}
 	putchar('\n');
 	return (0);
