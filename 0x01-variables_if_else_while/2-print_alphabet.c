7b1386a6c9175e1a64a4a229f8fef8765a552f8d
ZakariaOuakrim <zakarialukyguy@gmail.com>
2023-06-18 14:10:01 +0000

    qksjbdqk

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,10 @@ int main(void)
 {
 	char t[26] = "abcdefghijklmnopqrstuvwxyz";
 
-	for(int i=0;i<26;i++)
+	for (int i = 0; i < 26 ; i++)
 	{
 		putchar(t[i]);
-	} 
+	}
+	putchar('\n');
 	return (0);
 }
